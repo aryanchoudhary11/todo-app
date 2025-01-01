e6f101dce7e5a51de93a4db46c1da794c0efb108
Aryan <119430003+aryanchoudhary11@users.noreply.github.com>
2025-01-01 15:15:26 +0530

    Initial commit

---
+++ Todo-app/src/components/TodoItem2.jsx
@@ -0,0 +1,16 @@
+function TodoItem2(){
+    return (
+        <div className="container">
+      <div class="row">
+        <div class="col-4">Meet Arun</div>
+        <div class="col-4">03/01/25</div>
+        <div class="col-2">
+          <button type="button" class="btn btn-danger">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+    );
+}
+export default TodoItem2;
